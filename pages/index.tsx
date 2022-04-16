7d03a92b62077e3e0de69c2b9f6040fb21a84282
take917 <1tktk2tk3@gmail.com>
2022-04-16 16:59:28 +0900

    ✨  スタイリングの実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,29 @@ const Home: NextPage = () => {
   return (
     <div className="w-96 mx-auto p-20">
       <h1 className="text-xl font-bold">Todo</h1>
-      <div>
+      <div className="flex gap-x-2">
         <input type="text" className="border border-black" />
-        <button className="border border-black">ボタン</button>
+        <button className="border border-black shrink-0 px-2">追加</button>
       </div>
-      <ul>
-        <li>TODO1</li>
-        <li>TODO2</li>
-        <li>TODO3</li>
+      <ul className="mt-4 space-y-2">
+        <li>
+          <label className="flex item-center gap-x-2">
+            <input type="checkbox" />
+            <span>TODO1</span>
+          </label>
+        </li>
+        <li>
+          <label className="flex item-center gap-x-2">
+            <input type="checkbox" />
+            <span>TODO2</span>
+          </label>
+        </li>
+        <li>
+          <label className="flex item-center gap-x-2">
+            <input type="checkbox" />
+            <span>TODO3</span>
+          </label>
+        </li>
       </ul>
     </div>
   );
